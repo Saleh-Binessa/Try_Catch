dba4b8cc3d1341e0881ef03d0a479b6ed93ea1c1
Saleh-Binessa <83325@kfh.com>
2024-01-09 10:41:06 +0300

    tryCatchBonus2

---
+++ tryCatchBonus2/src/Bonus2.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Bonus2 {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        String validUsername = "Coded";
+        String validPassword = "Coded123";
+
+        for (int i = 0; i <= 4; i++) {
+
+            System.out.print("Username: ");
+            String usernameInput = scanner.nextLine();
+
+            System.out.print("Password: ");
+            String passwordInput = scanner.nextLine();
+
+            try {
+                if (usernameInput.equals(validUsername) && passwordInput.equals(validPassword)) {
+                    System.out.println("You are successfully logged in");
+                    break;
+                } else if (i < 4) {
+                    throw new Exception("Invalid credentials");
+                } else {
+                    throw new Exception("Maximum attempts exceeded");
+                }
+
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        scanner.close();
+    }
+}
